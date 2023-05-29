21f88e3bb1399e43c5c684c4f240c8496c9f89b2
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-05-29 11:58:03 +0800

    JSON serde

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// use serde::{Deserialize, Serialize};
+use serde_json::Value;
+// use std::fs::File;
+
+fn untyped_example(data: &str) -> Value {
+    serde_json::from_str(data).unwrap()
+}
 fn main() {
-    println!("Hello, world!");
+    let data = r#"
+	{
+		"name": "John Doe",
+		"age": 43,
+		"phones": [
+			"+44 1234567",
+			"+44 2345678"
+		]
+	}"#;
+    let v = untyped_example(data);
+    println!("Please call {} at the number {}", v["name"], v["phones"][0]);
 }
