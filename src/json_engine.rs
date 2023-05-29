0a6f95d8a95f90ebd6a4d2d18e2f79cfd20c9978
Shramee Srivastav <shramee.srivastav@gmail.com>
2023-05-29 15:06:02 +0800

    Add Hanji
    With custom template engine

---
+++ src/json_engine.rs
@@ -0,0 +1,210 @@
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
+
+use hanji::SyntaxGroup;
+use hanji::SyntaxKind;
+use hanji::SyntaxKind::*;
+use hanji::SyntaxNode;
+
+use hanji::TemplateEngine;
+
+pub struct JSONEngine {
+    pub templates: HashMap<String, String>,
+    nodes: Vec<(SyntaxKind, String, usize)>,
+    tokens: Vec<(SyntaxKind, String, String)>,
+    pub ignored_nodes: HashMap<SyntaxKind, u8>,
+    payload: String,
+}
+
+impl TemplateEngine for JSONEngine {
+    fn init(&mut self, _db: &dyn SyntaxGroup) {}
+
+    fn token(&mut self, description: &str, text: &str, node: &SyntaxNode, db: &dyn SyntaxGroup) {
+        let kind = node.kind(db);
+        if self.ignored_nodes.contains_key(&kind) {
+            return;
+        }
+        let text = match kind {
+            SyntaxKind::TokenNewline => ".",
+            _ => text,
+        };
+
+        self.tokens.push((kind, description.into(), text.into()));
+    }
+
+    fn node_start(&mut self, description: &str, node: &SyntaxNode, db: &dyn SyntaxGroup) {
+        let kind = node.kind(db);
+        if self.ignored_nodes.contains_key(&kind) {
+            return;
+        }
+
+        self.nodes
+            .push((kind, description.to_string(), self.tokens.len()));
+    }
+
+    fn node_end(&mut self, _description: &str, node: &SyntaxNode, db: &dyn SyntaxGroup) {
+        let kind = node.kind(db);
+        if self.ignored_nodes.contains_key(&kind) {
+            return;
+        }
+        let node_tup = self.nodes.pop().unwrap();
+        match kind {
+            FunctionWithBody => self.process_function_doc(node_tup, node, db),
+            _ => {}
+        }
+    }
+
+    fn get_result(&self) -> String {
+        self.payload.to_string()
+    }
+}
+
+impl JSONEngine {
+    pub fn new() -> Self {
+        let mut ignored_nodes: HashMap<SyntaxKind, u8> = HashMap::new();
+        // ignored_nodes.contains_key("");
+        // ignored_nodes.insert(ItemList, 0);
+        ignored_nodes.insert(TokenNewline, 0);
+        ignored_nodes.insert(SyntaxFile, 0);
+        // ignored_nodes.insert(Trivia, 0);
+        ignored_nodes.insert(TokenWhitespace, 0);
+        ignored_nodes.insert(TokenNewline, 0);
+        ignored_nodes.insert(TokenNewline, 0);
+        Self {
+            templates: HashMap::new(),
+            nodes: Vec::new(),
+            tokens: Vec::new(),
+            ignored_nodes,
+            payload: "".into(),
+        }
+    }
+
+    pub fn process_function_doc(
+        &mut self,
+        node_tup: (SyntaxKind, String, usize),
+        node: &SyntaxNode,
+        db: &dyn SyntaxGroup,
+    ) {
+        // Gets all children nodes
+        let tokens = &self.tokens[node_tup.2..];
+        let max_index = tokens.len();
+        let mut i: usize = 0;
+
+        let mut function_name = String::new();
+        let mut function_comments = String::new();
+        let mut function_args = String::new();
+        let mut function_return = String::new();
+
+        while i < max_index {
+            let (kind, _desc, text) = &tokens[i];
+            if TokenSingleLineComment != *kind {
+                break;
+            }
+            function_comments.push_str(&text.to_string().replace("//", "").trim());
+            function_comments.push_str("\n");
+            i += 1;
+        }
+
+        if !function_comments.is_empty() {
+            function_comments = format!("\n{function_comments}\n");
+        }
+
+        while i < max_index {
+            let (kind, _, _) = &tokens[i];
+            if TokenFunction == *kind {
+                i += 1;
+                break;
+            }
+
+            i += 1;
+        }
+
+        function_name.push_str(&tokens[i].2);
+
+        while i < max_index {
+            let (kind, _, _) = &tokens[i];
+            if TokenLParen == *kind {
+                i += 1;
+                break;
+            }
+            i += 1;
+        }
+
+        while i < max_index {
+            let (kind, _desc, text) = &tokens[i];
+            if TokenRParen == *kind {
+                i += 1;
+                break;
+            }
+            match kind {
+                TokenComma => {
+                    function_args.push_str("\n");
+                }
+                _ => {
+                    function_args.push_str(text);
+                    function_args.push_str(" ");
+                }
+            }
+            i += 1;
+        }
+
+        if !function_args.is_empty() {
+            function_args = format!("\n\n#### Parameters:\n{function_args}\n");
+        }
+
+        while i < max_index {
+            let (kind, _desc, text) = &tokens[i];
+            if TokenLBrace == *kind {
+                break;
+            }
+            match kind {
+                TokenArrow => {
+                    function_return = "".into();
+                }
+                _ => {
+                    function_return.push_str(text);
+                    function_return.push(' ');
+                }
+            }
+            i += 1;
+        }
+
+        if !function_return.is_empty() {
+            function_return = format!("\n\n#### Returns:\n{function_return}\n");
+        }
+
+        let mut code = "".to_string();
+        node.children(db)
+            .for_each(|x| code.push_str(&x.get_text(db)));
+        code = code.trim_matches('\n').to_string();
+
+        let hash = calculate_hash(&code);
+        if function_comments.trim().len() == 0 {
+            self.payload.push_str(&format!("'{hash}':`{code}`,\n"));
+        }
+    }
+
+    pub fn render_syntax_doc(
+        &self,
+        kind: SyntaxKind,
+        desc: &str,
+        text: &str,
+        _parent_kind: SyntaxKind,
+    ) -> String {
+        match kind {
+            TokenSingleLineComment => {
+                format!("{kind:#?} {desc} {text}\n")
+            }
+            _ => {
+                format!("{kind:#?} {desc} {text}\n")
+            }
+        }
+    }
+}
+
+fn calculate_hash<T: Hash>(t: &T) -> u64 {
+    let mut s = DefaultHasher::new();
+    t.hash(&mut s);
+    s.finish()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,87 @@
 // use serde::{Deserialize, Serialize};
 use serde_json::Value;
+mod json_engine;
 // use std::fs::File;
 
+use std::fs::{create_dir_all, File, OpenOptions};
+use std::io::prelude::*;
+use std::path::PathBuf;
+
+use hanji::run_printer;
+use hanji::utils::get_cairo_files_in_path;
+fn main() {
+    let out_path = PathBuf::from("out");
+
+    let repos = vec![
+        "greged93/2wrds_cntrcts",
+    ];
+
+    create_dir_all(&out_path).unwrap();
+    let mut doc_file_path = out_path.clone();
+    doc_file_path.push("functions-source");
+    doc_file_path.set_extension("js");
+    create_dir_all(&doc_file_path.parent().unwrap()).unwrap();
+
+    let mut file = OpenOptions::new()
+        .append(true)
+        .create(true)
+        .open(&doc_file_path.into_os_string().to_str().unwrap())
+        .unwrap();
+    for (i, repo) in repos.iter().enumerate() {
+        // let mut json_path = out_path.clone();
+        println!("{i} Processing {repo}");
+        file = process_files(format!("cairo-repos/{repo}"), repo, file);
+    }
+
+    // Cairo language spec
+    // cairo_lang_syntax_codegen::cairo_spec::get_spec
+}
+
+fn process_files(path: String, repo: &str, mut file: File) -> File {
+    let in_path = PathBuf::from(&path);
+
+    println!("{in_path:?}");
+
+    // remove_dir_all(&out_path).unwrap();
+    let cairo_files = get_cairo_files_in_path(&in_path);
+
+    let mut functions = String::from("\n// ");
+
+    functions.push_str(repo);
+    functions.push_str("\n");
+
+    for cairo_file in cairo_files.iter() {
+        println!("Processing {cairo_file:?}");
+        functions.push_str(&run_printer(
+            cairo_file.to_str().unwrap(),
+            hanji::MarkdownEngine::new(),
+        ));
+    }
+
+    file.write_all(functions.as_bytes()).unwrap();
+
+    file
+}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 fn untyped_example(data: &str) -> Value {
     serde_json::from_str(data).unwrap()
 }
-fn main() {
+fn main2() {
     let data = r#"
 	{
 		"name": "John Doe",
